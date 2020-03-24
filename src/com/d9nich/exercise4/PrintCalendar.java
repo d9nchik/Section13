a752dbe1fe1f971baaf3e7fa1d3aea9030aa9042
Danylo Halaiko <dan.galaiko@gmail.com>
2020-03-24 16:44:48 +0200

    PrintCalendar (exercise4) has been added.

---
+++ src/com/d9nich/exercise4/PrintCalendar.java
@@ -0,0 +1,51 @@
+package com.d9nich.exercise4;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class PrintCalendar {
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            System.out.println("Usage PrintCalendar month year");
+            System.exit(1);
+        }
+        printMonth(Integer.parseInt(args[1]), Integer.parseInt(args[0]));
+    }
+
+    public static void printMonth(int year, int month) {
+        printMonthTitle(year, month);
+        printMonthBody(year, month);
+    }
+
+    public static void printMonthTitle(int year, int month) {
+        String[] monthNames = {"January", "February", "March", "April", "May", "June", "July", "August", "September",
+                "October", "November", "December"};
+        System.out.println("\t\t" + monthNames[month - 1]
+                + " " + year);
+        System.out.println("−−−−−−−−−−−−−−−−−−−−−−−−−−−−−");
+        System.out.println(" Sun Mon Tue Wed Thu Fri Sat");
+    }
+
+    public static void printMonthBody(int year, int month) {
+        GregorianCalendar gregorianCalendar = new GregorianCalendar();
+        gregorianCalendar.set(Calendar.MONTH, month - 1);
+        gregorianCalendar.set(Calendar.YEAR, year);
+        gregorianCalendar.set(Calendar.DATE, 1);
+        int startDay = gregorianCalendar.get(Calendar.DAY_OF_WEEK) - 1;
+
+        int numberOfDaysInMonth = gregorianCalendar.getActualMaximum(Calendar.DAY_OF_MONTH);
+
+        int i;
+        for (i = 0; i < startDay; i++)
+            System.out.print(" ");
+
+        for (i = 1; i <= numberOfDaysInMonth; i++) {
+            System.out.printf("%4d", i);
+
+            if ((i + startDay) % 7 == 0)
+                System.out.println();
+        }
+
+        System.out.println();
+    }
+}
